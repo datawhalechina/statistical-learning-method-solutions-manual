1d96c0689c7b24638c29b8471f24955445790c2f
Relph1119 <huruifeng1202@163.com>
2021-08-14 03:22:09 +0800

    完善第9章习题9.1、习题9.2

---
+++ codes/ch09/three_coin_EM.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: three_coin_EM.py
+@time: 2021/8/14 0:56
+@project: statistical-learning-method-solutions-manual
+@desc: 习题9.1 三硬币模型的EM算法
+"""
+
+import math
+
+
+class ThreeCoinEM:
+    def __init__(self, prob, tol=1e-6, max_iter=1000):
+        """
+        初始化模型参数
+        :param prob: 模型参数的初值
+        :param tol: 收敛阈值
+        :param max_iter: 最大迭代次数
+        """
+        self.prob_A, self.prob_B, self.prob_C = prob
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def calc_mu(self, j):
+        """
+        （E步）计算mu
+        :param j: 观测数据y的第j个
+        :return: 在模型参数下观测数据yj来自掷硬币B的概率
+        """
+        # 掷硬币A观测结果为正面
+        pro_1 = self.prob_A * math.pow(self.prob_B, data[j]) * math.pow((1 - self.prob_B), 1 - data[j])
+        # 掷硬币A观测结果为反面
+        pro_2 = (1 - self.prob_A) * math.pow(self.prob_C, data[j]) * math.pow((1 - self.prob_C), 1 - data[j])
+        return pro_1 / (pro_1 + pro_2)
+
+    def fit(self, data):
+        count = len(data)
+        print("模型参数的初值：prob_A={}, prob_B={}, prob_C={}".format(self.prob_A, self.prob_B, self.prob_C))
+        print("EM算法训练过程：")
+        for i in range(self.max_iter):
+            # （E步）得到在模型参数下观测数据yj来自掷硬币B的概率
+            _mu = [self.calc_mu(j) for j in range(count)]
+            # （M步）计算模型参数的新估计值
+            prob_A = 1 / count * sum(_mu)
+            prob_B = sum([_mu[k] * data[k] for k in range(count)]) \
+                     / sum([_mu[k] for k in range(count)])
+            prob_C = sum([(1 - _mu[k]) * data[k] for k in range(count)]) \
+                     / sum([(1 - _mu[k]) for k in range(count)])
+            print('第{}次：prob_A={:.4f}, prob_B={:.4f}, prob_C={:.4f}'.format(i + 1, prob_A, prob_B, prob_C))
+            # 计算误差值
+            error = abs(self.prob_A - prob_A) + abs(self.prob_B - prob_B) + abs(self.prob_C - prob_C)
+            self.prob_A = prob_A
+            self.prob_B = prob_B
+            self.prob_C = prob_C
+            # 判断是否收敛
+            if error < self.tol:
+                print("模型参数的极大似然估计：prob_A={:.4f}, prob_B={:.4f}, prob_C={:.4f}".format(self.prob_A, self.prob_B,
+                                                                                       self.prob_C))
+                break
+
+
+if __name__ == '__main__':
+    # 加载数据
+    data = [1, 1, 0, 1, 0, 0, 1, 0, 1, 1]
+    # 模型参数的初值
+    init_prob = [0.46, 0.55, 0.67]
+
+    # 三硬币模型的EM模型
+    em = ThreeCoinEM(prob=init_prob, tol=1e-5, max_iter=100)
+    # 模型训练
+    em.fit(data)
